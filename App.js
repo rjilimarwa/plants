18617c8d3e8e86af5d0409cad99ab0d0e1b1d687
rjilimarwadjerba@gmail.com <admin@exemple.com>
2020-04-22 09:38:55 +0100

    add Category List -add page Product -add page Settings -add page Login -add page SignUp -add page Forgot -add page Welcome -add page Browse

--- App.js
+++ App.js
@@ -1,64 +1,35 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet,View,Text } from "react-native";
 
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
-
-import Navigation from "./navigation";
-import { Block } from "./components";
-
+import Search from './components/Search'
+import About from './components/About'
+import { TabNavigator } from "react-navigation";
 // import all used images
-const images = [
-   require("./assets/images/explore_1.png"),
-   require("./assets/images/explore_2.png"),
-   require("./assets/images/explore_3.png"),
-   require("./assets/images/explore_4.png"),
-   require("./assets/images/explore_5.png"),
-   require("./assets/images/explore_6.png"),
-   require("./assets/images/illustration_1.png"),
-   require("./assets/images/illustration_2.png"),
-   require("./assets/images/illustration_3.png"),
-   require("./assets/icons/back.png"),
-   require("./assets/icons/plants.png"),
-   require("./assets/icons/fertilizers.png"),
-   require("./assets/icons/pots.png"),
-   require("./assets/icons/sprayers.png")
-];
+
+const Tabs = TabNavigator({
+    Search:{ screen: Search },
+    About:{ screen: About }
+});
 
 export default class App extends React.Component {
 
-         state={
-             isLoadingComplete: false
-          };
-    handleResourcesAsync = async () => {
-    // we're caching all the images
-    // for better performance on the app
 
-    const cacheImages = images.map(image => {
-            return Asset.fromModule(image).downloadAsync();
-});
-
-return Promise.all(cacheImages);
-};
-
-
-
-render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-        return (
-            <AppLoading
-        startAsync={this.handleResourcesAsync}
-        onError={error => console.warn(error)}
-        onFinish={() => this.setState({ isLoadingComplete: true })}
-    />
+render(){
+    return(
+        <Tabs/>
     );
-    }
-    return (
-        <Block >
-    <Navigation />
-    </Block>
-);
 }
-};
+}
+const styles=StyleSheet.create({
+        container:{
+            alignItem:"center",
+            flex:1,
+            backgroundColor:'#fff',
+            marginHorizontal:40
 
+        }
+}
 
+);
